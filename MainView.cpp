c1d260d32e96675f20812cba9a446c8df91ad1f9
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-10-17 20:37:49 +0200

    Fix crashing after clicking  'Create new task' button twice

--- MainView.cpp
+++ MainView.cpp
@@ -2,9 +2,9 @@
 
 MainView::MainView() : wxFrame(nullptr, wxID_ANY, "Task manager")
 {
-	taskCreateView = new TaskCreateView(this);
 	taskCreateViewButton = new wxButton(this, wxID_ANY, "Create new task");
 	taskCreateViewButton->Bind(wxEVT_BUTTON, [=](wxCommandEvent&) {
+		taskCreateView = new TaskCreateView(this);
 		taskCreateView->Show();
 		});
 }
