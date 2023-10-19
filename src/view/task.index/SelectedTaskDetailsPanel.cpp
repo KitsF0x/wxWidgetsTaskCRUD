2ffb4c8bd3787a5c53bd8f743d023b6e3496c7cd
Bartłomiej Markiewicz <bartekmarkiewicz1799+githubacc@gmail.com>
2023-10-19 20:10:51 +0200

    Remove unused update method

--- src/view/task.index/SelectedTaskDetailsPanel.cpp
+++ src/view/task.index/SelectedTaskDetailsPanel.cpp
@@ -3,10 +3,12 @@
 SelectedTaskDetailsPanel::SelectedTaskDetailsPanel(wxWindow* parrent)
 	: wxPanel(parrent, wxID_ANY)
 {
+	selectedTaskId = new wxStaticText(this, wxID_ANY, TASK_ID_LABEL);
 	selectedTaskTitle = new wxStaticText(this, wxID_ANY, TASK_TITLE_LABEL);
 	selectedTaskDescription = new wxStaticText(this, wxID_ANY, TASK_DESCRIPTION_LABEL);
 
 	wxStaticBoxSizer* sizer = new wxStaticBoxSizer(wxVERTICAL, this);
+	sizer->Add(selectedTaskId);
 	sizer->Add(selectedTaskTitle);
 	sizer->Add(selectedTaskDescription);
 	SetSizer(sizer);
@@ -14,6 +16,7 @@ SelectedTaskDetailsPanel::SelectedTaskDetailsPanel(wxWindow* parrent)
 
 void SelectedTaskDetailsPanel::updateDetailsFromTask(Task task)
 {
+	selectedTaskId->GetLabelText(TASK_ID_LABEL + std::to_string(task.id));
 	selectedTaskTitle->SetLabelText(TASK_TITLE_LABEL + task.title);
 	selectedTaskDescription->SetLabelText(TASK_DESCRIPTION_LABEL + task.description);
 }
--- src/view/task.index/SelectedTaskDetailsPanel.hpp
+++ src/view/task.index/SelectedTaskDetailsPanel.hpp
@@ -6,9 +6,11 @@
 class SelectedTaskDetailsPanel : public wxPanel
 {
 private:
+	wxStaticText* selectedTaskId;
 	wxStaticText* selectedTaskTitle;
 	wxStaticText* selectedTaskDescription;
 
+	const std::string TASK_ID_LABEL{ "Task id in repository: " };
 	const std::string TASK_TITLE_LABEL{ "Task title: " };
 	const std::string TASK_DESCRIPTION_LABEL{ "Task description: " };
 public:
